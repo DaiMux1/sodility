858dee402f1337844d31ab1a39f3c0f2289c3e34
DaiLV <dai.levan@ddsolution.vn>
2022-05-04 18:16:40 +0700

    Provider

--- deploy.js
+++ deploy.js
@@ -1,1 +1,9 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    "crime upon pet number coast forest silly enhance snake believe mind grief",
+    'https://rinkeby.infura.io/v3/07a29ee61f8c456a8eae14bd2d9556b0'
+)
+const web3 = new Web3(provider)
